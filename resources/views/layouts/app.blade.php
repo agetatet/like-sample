6ec43dd40dc69ef8b1e563d8eaaa3c6b5401c7e4
津田陽香 <agetate.tsuda@gmail.com>
2023-03-28 10:15:00 +0900

    ヘッダータイトル変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 <body>
     <div id="app">
         <header class="bgMainColor">
-            <small class="ps-3 text-sm"><b>harublog</b><span class="ps-3">いいね機能を実装する（localStorage）</span></small>
+            <small class="ps-3 text-sm"><b>harublog</b><span class="ps-3">いいね機能を実装する（localStorage × Vue.js（CDN））</span></small>
         </header>
         @yield('content')
         <div class="flex justify-center my-3">
